1001177268c590c93b3e53b2424250cf59a25fcf
Glen Campbell <glen@broadpool.com>
2015-10-28 12:37:32 -0700

    updated

--- publish.sh
+++ publish.sh
@@ -2,7 +2,7 @@
 
 if [ -d site ]; then
     cd site
-    aws s3 sync . s3://broadpool.com --cache-control "max-age=600"
+    aws s3 sync . s3://broadpool.xyz --cache-control "max-age=600"
 else
     echo "You're not in the right directory"
     exit
